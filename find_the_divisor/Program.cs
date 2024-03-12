ded609c88390a2d6723b15fd44322b7c7a3c9a9f
Felipe Scheidemantel <me@felipe.app>
2024-03-13 00:50:42 +0100

    solution to "find the divisor"

---
+++ find_the_divisor/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Kata
+{
+	public static int[]? Divisors(int n)
+	{
+		List<int> divisors = new List<int>();
+
+		for (int i = 2; i < n; i++) {
+			if (n % i == 0) {
+				divisors.Add(i);
+			}
+		}
+
+		if (divisors.Count > 0) {
+			return divisors.ToArray();
+		} else {
+			if (n != 1) {
+				Console.WriteLine($"{n} is prime");
+			}
+			return null;
+		}
+	}
+
+	public static void Main(string[] args)
+	{
+		foreach(var str in args)
+		{
+			int tryInt;
+
+			if (int.TryParse(str, out tryInt)) {
+				int[]? divisors = Divisors(tryInt);
+
+				if (divisors?.Length > 0) {
+					Console.WriteLine($"The divisors of {tryInt} are: ");
+					foreach (var d in divisors ?? new int[0])
+					{
+						Console.WriteLine(d);
+					}
+				}
+			}
+		}
+	}
+}

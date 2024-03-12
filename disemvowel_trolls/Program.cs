028d47124900e84d7d697617685bd348f56f0ec7
Felipe Scheidemantel <me@felipe.app>
2024-03-12 23:29:33 +0100

    use regex to filter out vowels

--- disemvowel_trolls/Program.cs
+++ disemvowel_trolls/Program.cs
@@ -1,8 +1,11 @@
-﻿public static class Kata
+﻿using System.Text.RegularExpressions;
+
+public static class Kata
 {
 	public static string Disemvowel(string str)
 	{
-		return str;
+		string pattern = "[aeiou]";
+		return Regex.Replace(str, pattern, String.Empty, RegexOptions.IgnoreCase);
 	}
 
 

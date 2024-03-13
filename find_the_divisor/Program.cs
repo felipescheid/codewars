481cef9c316f4500483fdb0e8ca4eb6b674ffad3
Felipe Scheidemantel <me@felipe.app>
2024-03-13 01:09:05 +0100

    optimize solution for find the divisors

--- find_the_divisor/Program.cs
+++ find_the_divisor/Program.cs
@@ -4,14 +4,17 @@
 	{
 		List<int> divisors = new List<int>();
 
-		for (int i = 2; i < n; i++) {
+		for (int i = 2; i <= Math.Sqrt(n); i++) {
 			if (n % i == 0) {
 				divisors.Add(i);
+				if (i != n / i) {
+					divisors.Add(n / i);
+				}
 			}
 		}
 
 		if (divisors.Count > 0) {
-			return divisors.ToArray();
+			return divisors.OrderBy(x => x).ToArray();
 		} else {
 			if (n != 1) {
 				Console.WriteLine($"{n} is prime");

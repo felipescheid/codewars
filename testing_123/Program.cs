5bada22cb23073f18c4f1866ae70e3155b126580
Felipe Scheidemantel <me@felipe.app>
2024-03-19 20:28:38 +0100

    implement solution with LINQ

---
+++ testing_123/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * This is my first start using Linq! I think that using Select in this context is very similar
+ * to the map operation in Javascript.
+ */
+public class Program
+{
+	public static List<string> Number(List<string> lines)
+	{
+		return lines.Select((line, index) => $"{index + 1}: {line}").ToList();
+	}
+
+	static int Main(string[] args)
+	{
+		List<string> lines = args.ToList();
+
+		foreach(var word in Number(lines))
+		{
+			Console.WriteLine(word);
+		}
+		return 1;
+	}
+}
